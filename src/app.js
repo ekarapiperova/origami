e17f126a995ef1c11d9a6bab3b9bf9e5e53c5ec1
ekarapiperova <59848353+ekarapiperova@users.noreply.github.com>
2020-07-13 13:39:50 +0300

    link component
    navigation fix css

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/header/index'
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
